bd6359394fddc7d3e59c7c1a6ad10245a5d4e42e
hjp4lucifer <hjp_0002@163.com>
2014-09-09 11:19:22 +0800

    测试常量

--- src/main/java/cn/lucifer/demo/string/ArrayTest.java
+++ src/main/java/cn/lucifer/demo/string/ArrayTest.java
@@ -21,6 +21,31 @@ public class ArrayTest {
 	public void getSimpleByIds(Collection<String> photoIds,
 			boolean includePrivate, String... fields) {
 		System.out.println(fields.length);
-		System.out.println( ~0);
+		System.out.println(~0);
+	}
+
+	@Test
+	public void testRunArray() {
+		System.out.println("start:\t" + Runtime.getRuntime().freeMemory());
+		runArray();
+		System.out.println("end:\t" + Runtime.getRuntime().freeMemory());
+	}
+
+	@Test
+	public void testRunArray4Loop() {
+		long startFreeMemory = Runtime.getRuntime().freeMemory();
+		for (int i = 0; i < 10000; i++) {
+			System.out.print(i + "-");
+			testRunArray();
+		}
+		long endFreeMemory = Runtime.getRuntime().freeMemory();
+		System.out.println(String.format(
+				"result: startFreeMemory=%d, endFreeMemory=%d",
+				startFreeMemory, endFreeMemory));
+	}
+
+	private void runArray() {
+		int[][] array = new int[256][256];
+		System.out.println(array[1][3]);
 	}
 }
