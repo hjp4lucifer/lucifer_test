75c86385b8dae63672e6196931d2ffe514531f2e
hjp4lucifer <hjp_0002@163.com>
2014-10-09 16:30:34 +0800

    weibo test

---
+++ src/main/java/cn/lucifer/demo/weibo/SimpleTest.java
@@ -0,0 +1,24 @@
+package cn.lucifer.demo.weibo;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.junit.Test;
+
+import cn.lucifer.util.HttpClientHelper;
+
+public class SimpleTest {
+
+	protected final String access_token = "2.00pebQzBjguruB8f5e5c144cI9a8jD";
+	protected final String uid = "1756456945";
+
+	@Test
+	public void testUsersShow() {
+		String url = "https://api.weibo.com/2/users/show.json";
+		NameValuePair[] parametersBody = new NameValuePair[] {
+				new NameValuePair("access_token", access_token),
+				new NameValuePair("uid", uid) };
+
+		byte[] data = HttpClientHelper.httpGet(url, parametersBody);
+		System.out.println(new String(data));
+	}
+
+}
