ce63a4467396540686d4e7d31dd49c56ca3fe45e
Lucifer <hjp_0002@163.com>
2021-01-02 15:18:01 +0800

    修改视频合并脚本

--- src/main/java/cn/lucifer/demo/avs/VideoMerge.java
+++ src/main/java/cn/lucifer/demo/avs/VideoMerge.java
@@ -10,34 +10,23 @@ import java.util.Comparator;
 
 public class VideoMerge {
 
-    public String generateVideoAvs(String folderName, final String suffix) {
+    private int beginIndex;
+    private int endIndex;
+
+    public VideoMerge(int beginIndex, int endIndex) {
+        this.beginIndex = beginIndex;
+        this.endIndex = endIndex;
+    }
+
+    public String generateVideoAvs(String folderName, String suffix) {
         StrBuilder builder = new StrBuilder();
-        builder.append("LoadPlugin(\"C:\\lucifer_soft\\MeGUI\\tools\\lsmash\\LSMASHSource.dll\")").append('\n');
+        builder.append("LoadPlugin(\"C:\\lucifer_soft\\MeGUI-2913\\tools\\lsmash\\LSMASHSource.dll\")").append('\n');
 
-        File folder = new File(folderName);
-        File[] videoFileList = folder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(suffix);
-            }
-        });
-
-        if (null == videoFileList || 0 == videoFileList.length) {
-            throw new RuntimeException("该目录没有匹配的视频文件, folderName=" + folderName);
-        }
-
-        Arrays.sort(videoFileList, new Comparator<File>() {
-
-            @Override
-            public int compare(File o1, File o2) {
-                String fn1 = StringUtils.removeEnd(o1.getName(), suffix);
-                String fn2 = StringUtils.removeEnd(o2.getName(), suffix);
-                return Integer.parseInt(fn1) - Integer.parseInt(fn2);
-            }
-        });
+        File[] videoFileList = getVideoFiles(folderName, suffix);
 
         StrBuilder videoMethod = new StrBuilder("video=");
-        for (int i = 0; i < videoFileList.length; i++) {
+        int endIndex = getEndIndex(videoFileList);
+        for (int i = beginIndex; i <= endIndex; i++) {
             File videoFile = videoFileList[i];
             String paramName = "v" + i;
             builder.append(String.format("%s=LWLibavVideoSource(\"%s\")", paramName, videoFile.getPath())).append('\n');
@@ -48,6 +37,47 @@ public class VideoMerge {
         builder.append("return video");
 
         return builder.toString();
+    }
+
+    protected File[] getVideoFiles(String folderName, String suffix) {
+        File folder = new File(folderName);
+        File[] videoFileList = folder.listFiles((dir, name) -> name.endsWith(suffix));
+
+        if (null == videoFileList || 0 == videoFileList.length) {
+            throw new RuntimeException("该目录没有匹配的视频文件, folderName=" + folderName);
+        }
+
+        Arrays.sort(videoFileList, (o1, o2) -> {
+            String fn1 = StringUtils.removeEnd(o1.getName(), suffix);
+            String fn2 = StringUtils.removeEnd(o2.getName(), suffix);
+            return Integer.parseInt(fn1) - Integer.parseInt(fn2);
+        });
+        return videoFileList;
+    }
+
+    public String generateAudioAvs(String folderName, String suffix) {
+        StrBuilder builder = new StrBuilder();
+        builder.append("LoadPlugin(\"C:\\lucifer_soft\\MeGUI\\tools\\ffms\\ffms2.dll\")").append('\n');
+
+        File[] videoFileList = getVideoFiles(folderName, suffix);
+
+        StrBuilder videoMethod = new StrBuilder("audio=");
+        int endIndex = getEndIndex(videoFileList);
+        for (int i = beginIndex; i <= endIndex; i++) {
+            File videoFile = videoFileList[i];
+            String paramName = "a" + i;
+            builder.append(String.format("%s=FFAudioSource(\"%s\", track=1)", paramName, videoFile.getPath())).append('\n');
+            videoMethod.append(paramName).append("+");
+        }
+        builder.append(videoMethod.substring(0, videoMethod.size() - 1)).append('\n');
+        builder.append("return audio");
+
+        return builder.toString();
+    }
+
+
+    private int getEndIndex(File[] videoFileList) {
+        return Math.min(this.endIndex, videoFileList.length - 1);
     }
 
 }
--- src/test/java/cn/lucifer/demo/avs/VideoMergeTest.java
+++ src/test/java/cn/lucifer/demo/avs/VideoMergeTest.java
@@ -1,18 +1,37 @@
 package cn.lucifer.demo.avs;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.StrBuilder;
 import org.junit.Test;
+
+import java.io.File;
 
 import static org.junit.Assert.*;
 
 public class VideoMergeTest {
 
-    @Test
-    public void generateVideoAvs() {
-        VideoMerge videoMerge = new VideoMerge();
-        String folderName = "E:\\百家讲坛·国史通鉴\\01 第一部\\20140103 国史通鉴（第一部）1 远古时代";
-        String suffix = ".ts";
-        String avsData = videoMerge.generateVideoAvs(folderName, suffix);
-        System.out.println(avsData);
+    private final static String suffix = ".ts";
 
+    private String folderName = "E:\\百家讲坛·国史通鉴\\04 两晋南北朝篇\\20170109 国史通鉴•两晋南北朝篇（19）南朝旧事";
+
+    private VideoMerge videoMerge = new VideoMerge(0, 30);
+
+    @Test
+    public void getVideoFiles() throws Exception {
+        String videoName = StringUtils.substringAfterLast(folderName, "\\");
+        String cmd = String.format("copy /b \"%s\\*.ts\" \"%s\\%s.ts\"", folderName, new File(folderName).getParent(), videoName);
+        System.out.println(cmd);
+
+        File[] videoFiles = videoMerge.getVideoFiles(folderName, suffix);
+        StrBuilder builder = new StrBuilder();
+        for (int i = 0; i < videoFiles.length; i++) {
+            File file = videoFiles[i];
+            file.renameTo(new File(file.getParentFile(), String.format("%03d.ts", i)));
+//            System.out.println(String.format("%03d.ts",i));
+        }
+//        System.out.println(builder.toString());
+//        Process ps = Runtime.getRuntime().exec("cmd /C " + cmd);
+//        ps.waitFor();
+//        System.out.println("finished cmd");
     }
 }
